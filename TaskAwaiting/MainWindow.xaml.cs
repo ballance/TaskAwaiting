3986860a1cb3603f7125764b09b3f33ef140da11
Chris Ballance <cballance@boardbooks.com>
2015-03-14 18:35:36 -0400

    Initial implementation of pub/sub. Toggles orange rectangle and writes a messsage to the screen

--- TaskAwaiting/MainWindow.xaml.cs
+++ TaskAwaiting/MainWindow.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Practices.Prism.PubSubEvents;
 
 namespace TaskAwaiting
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private IEventAggregator _eventAggregator;
+        private MainWindowViewModel _owner;
+
         public MainWindow()
         {
             InitializeComponent();
+            _eventAggregator = new EventAggregator();
+            _owner = new MainWindowViewModel(_eventAggregator);
+
+            this.DataContext = _owner;
+            _eventAggregator.GetEvent<UpdateTextboxEvent>().Subscribe(UpdateTextboxAction);
+
+            _eventAggregator.GetEvent<MoveRectangleEvent>().Subscribe(MoveRectangleActoin);
+        }
+
+        private void MoveRectangleActoin(object obj)
+        {
+            _owner.ToggleRectangle();
+        }
+
+        private void UpdateTextboxAction(object obj)
+        {
+            _owner.Output += "Oh Hai there! " + DateTime.Now +"\n";
         }
     }
 }
